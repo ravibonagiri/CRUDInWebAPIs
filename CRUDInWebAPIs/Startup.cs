3c50815612acfc4935ead9d7cd526a5dd17436b8
ravindharcbit <Ravindhar_cbit@outlook.com>
2021-10-23 17:32:10 +0530

    commit all and Sync - 2nd attempt

--- CRUDInWebAPIs/Startup.cs
+++ CRUDInWebAPIs/Startup.cs
@@ -13,7 +13,8 @@ namespace CRUDInWebAPIs
 {
     public class Startup
     {
-        // Changes made by RB
+        // Changes made by RB - 1st attempt
+        //Changes made 2nd attempt
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
